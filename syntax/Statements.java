4c5c3312518c5e3606a77ab876e14f8de0f03a3a
Omillo Fidel Charles <fidelomillo812@gmail.com>
2026-05-18 23:06:46 +0300

    statements

---
+++ syntax/Statements.java
@@ -0,0 +1,14 @@
+package syntax;
+
+public class Statements{
+    public static void main(String[] args){
+        //This is a statement to print hello world to the console
+        //A statement must end with a semi colon
+        System.out.println("Hello World!");
+
+        //You can also print multiple statements to the console
+        System.out.println("I am Omillo Fidel Charles");
+        System.out.println("I am a student");
+        System.out.println("I love coding!");
+    }
+}
