1fc9400d9fb5db129ddd4a8ca38ccb9e6c3a6a35
Omillo Fidel Charles <fidelomillo812@gmail.com>
2026-05-18 23:01:37 +0300

    intro

---
+++ intro/Intro.java
@@ -0,0 +1,8 @@
+package intro;
+
+public class Intro{
+    public static void main(String[] args){
+         String Greeting = "Hello";
+         System.out.println(Greeting + ", World!");
+    }
+}
