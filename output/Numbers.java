49b23dc6407802776bb667f78c5f3e846c72528f
Omillo Fidel Charles <fidelomillo812@gmail.com>
2026-05-18 23:18:35 +0300

    Output

---
+++ output/Numbers.java
@@ -0,0 +1,19 @@
+package output;
+
+public class Numbers{
+    public static void main(String[] args){
+        //You can print numbers to the console using the System.out.println() Method
+        //This prints the number 5 of type int to the console
+        System.out.println(5);
+
+        //You can also perform numerical expressions on numbers in the System.out.Println() method
+        //This will output 11 following the basic mathematical rules
+        System.out.println(5 + 5);
+        System.out.println(6 * 2); //Outputs 12
+        System.out.println(9 / 3); //outputs 3
+        System.out.println(17 - 12); //outputs 5
+
+        //Also in the event of multple numerical expressions BODMAS will apply
+        System.out.println(2 * 3 + 6 / 2); //This outputs 9
+    }
+}
---
+++ output/Text.java
@@ -0,0 +1,13 @@
+package output;
+
+public class Text{
+    public static void main(String[] args){
+        //Printing texts to the console is just straightforward -> Just use the full or double quotes
+        System.out.println("Hello World");
+        System.out.println("Welcome today!!");
+
+        //We can also use the System.out.print(), but a new line will not be inserted after every statement
+        System.out.print("Welcome to the Java Open Source Repository");
+        System.out.print("The best open source learning repo in the world!");
+    }
+}
