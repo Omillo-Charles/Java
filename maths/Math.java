7bb8944fe3df3abbc879ec18a7b3eb1540b9ed1c
Omillo Fidel Charles <fidelomillo812@gmail.com>
2026-05-19 10:54:24 +0300

    Math

---
+++ maths/Math.java
@@ -0,0 +1,18 @@
+package maths;
+
+public class Math{
+    public static void main(String[] args){
+        //The Math.max method is used to return the bigger number
+        // System.out.println(Math.max(23, 34));
+        // System.out.println(Math.min(37, 38));
+
+        //The rounding off methods
+        // System.out.println(Math.round(39.348));
+        // System.out.println(Math.ceil(378.34));
+        // System.out.println(Math.floor(43673.439));
+
+        //The Power and squareroot methods
+        // System.out.println(Math.pow(2, 5));
+        // System.out.println(Math.sqrt(64));
+    }
+}
