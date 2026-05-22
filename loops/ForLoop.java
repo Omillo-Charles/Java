6fa57711125052b7bb7d33e0a8aa13d43c67b4da
Omillo Fidel Charles <fidelomillo812@gmail.com>
2026-05-22 21:05:47 +0300

    loops

---
+++ loops/ForLoop.java
@@ -0,0 +1,12 @@
+package loops;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        // create an array of five uni names
+        String[] universityNames = { "Multimedia", "Strathmore", "Daystar" };
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(universityNames[i]);
+        }
+    }
+}
